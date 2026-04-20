0932dc6dbe6a9e484f100ce852425bb71735f131
Laurin Kerkloh <laurin@kerk-loh.de>
2026-04-20 10:21:53 +0200

    laurinpc: wait online

--- laurinpc.sh
+++ laurinpc.sh
@@ -9,8 +9,8 @@ sudo pacman -S --noconfirm --needed ddcutil
 #     echo "options nct6775 force_id=0xd802" | sudo tee /etc/modprobe.d/nct6775.conf
 # fi
 
-# sudo systemctl unmask systemd-networkd-wait-online.service
-# sudo systemctl enable systemd-networkd-wait-online.service
+sudo systemctl unmask systemd-networkd-wait-online.service
+sudo systemctl enable systemd-networkd-wait-online.service
 #
 # sudo sed -i '/^\s*RequiredForOnline\s*=/c\RequiredForOnline=unmanaged' /etc/systemd/network/20-wlan.network
 # sudo sed -i '/^\s*RequiredForOnline\s*=/c\RequiredForOnline=unmanaged' /etc/systemd/network/20-wwan.network
